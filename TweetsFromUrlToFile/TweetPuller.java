a5df69439ed4309501d43682dcab338eb77092c6
Damian Reichel <kingdamian42@gmail.com>
2014-04-21 10:47:57 -0400

    Added timer to java app

--- TweetsFromUrlToFile/TweetPuller.java
+++ TweetsFromUrlToFile/TweetPuller.java
@@ -4,6 +4,8 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.TimerTask;
+import java.util.Timer;
 
 
 public class TweetPuller{
@@ -14,16 +16,34 @@ public class TweetPuller{
     }
     String urlString = args[0];
     String fileName = args[1] + ".twtext";
-    URL url;
-  try {
-    url = new URL(urlString);
-      InputStream in = url.openStream();
-      ReadableByteChannel rbc = Channels.newChannel(in);
-      FileOutputStream fos = new FileOutputStream(fileName);
-      fos.getChannel().transferFrom(rbc,0,Long.MAX_VALUE);
-  } catch (IOException e) {
-    // TODO Auto-generated catch block
-    e.printStackTrace();
+    int interval = Integer.parseInt(args[2]);
+    MyTimerTask task = new MyTimerTask(urlString,fileName);
+    Timer timer = new Timer();
+    timer.schedule(task,0,interval);
   }
+
+  private static class MyTimerTask extends TimerTask{
+    String urlString;
+    String fileName;
+
+    public MyTimerTask(String url, String fileName){
+      this.urlString = url;
+      this.fileName = fileName;
+    }
+
+    public void run(){
+      try {
+        URL url;
+        url = new URL(urlString);
+        InputStream in = url.openStream();
+        ReadableByteChannel rbc = Channels.newChannel(in);
+        FileOutputStream fos = new FileOutputStream(fileName);
+        fos.getChannel().transferFrom(rbc,0,Long.MAX_VALUE);
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
   }
+
 }
